a7edddfec3558188c46ddc2dec2e046a333d6464
Victor Moreno <victormoreno@Victors-Laptop.local>
2024-10-08 21:22:24 -0500

    removing the lines for testing the orchestrator file

--- scripts/orchestrator.py
+++ scripts/orchestrator.py
@@ -46,31 +46,6 @@ class Orchestrator():
 
         return df1_.join(df2_.set_index(key),on=key,lsuffix='Terrazas',rsuffix='Licencias',how='inner')
 
-
-
-        
-if __name__=='__main__':
-    orchestrator = Orchestrator()
-    files_to_process = {'../data/Licencias_Locales_202104.csv':'../output/Licencias_SinDuplicados.csv'
-                        ,'../data/Terrazas_202104.csv':'../output/Terrazas_Normalizadas.csv'
-                        ,'../data/Locales_202104.csv':'../output/Locales_Procesado.csv'
-                        ,'../data/books.json':'../output/Books_Limpio.csv'}
-    orchestrator.process_in_batch(files=files_to_process)
-
-    #Data integration
-    ## Join entre datasets
-    df_terr = pd.read_csv('../output/Terrazas_Normalizadas.csv')
-    df_lic = pd.read_csv('../output/Licencias_SinDuplicados.csv')
-    df_joined = orchestrator.join_2_datasets(df_terr,df_lic)
-    df_joined.to_csv('../output/Licencias_Terrazas_Integradas.csv')
-
-    #Datos Geograficos
-    df_superficies_agg = (df_terr
-                          .groupby(by=['id_barrio_local'])
-                          .agg(superficie_total=('Superficie_ES','sum'))
-                          .reset_index()
-                          .sort_values(by=['superficie_total'],ascending = False)
-                          )
-    df_superficies_agg.to_csv('../output/Superficies_Agregadas.csv',index=False)
+    
 
 
